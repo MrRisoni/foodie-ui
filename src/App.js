253496f80303eb26c633de036601126aba35dde0
MrRisoni <vanguelisk@gmail.com>
2020-08-12 18:28:09 +0300

    render parts

--- src/App.js
+++ src/App.js
@@ -1,12 +1,57 @@
 import React from 'react';
+import Basket from './Basket';
+const masa = require('./data.json');
 
 function App() {
+  
   return (
-    <div className="App">
-<div class="alert alert-primary" role="alert">
-  A simple primary alert—check it out!
-</div>
-    </div>
+    <main>
+ 
+        <div class="row">
+          <div class="col-2">
+             Menu
+            </div>
+
+            <div class="col-8">
+
+            <div class="alert alert-primary" role="alert">
+  {masa.food.name}
+</div> 
+
+{masa.food.foodparts.map(fdPart => {
+                return (
+                  <div class="row">
+                  <div class="col-8">
+                    <div  key={fdPart.id} class="card">
+                <div class="card-header">
+                {fdPart.name}
+                </div>
+                <div class="card-body">
+                  <blockquote class="blockquote mb-0">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.</p>
+                    <footer class="blockquote-footer">Someone famous in <cite title="Source Title">Source Title</cite></footer>
+                  </blockquote>
+                </div>
+                </div>
+                </div>
+              </div>
+              )
+            })}
+
+
+
+            </div>
+
+
+     
+            <div class="col-2">
+              <Basket/>
+            </div>
+
+
+
+        </div>
+    </main>
   );
 }
 
---
+++ src/Basket.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Basket() {
+  return (
+    <div className="App">
+        Total <br/>
+        <button className="btn btn-warning">Checkout!</button>
+    </div>
+  );
+}
+
+export default Basket;
