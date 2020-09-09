a459904c21069a3e3fc678d439223d269215bb7c
MrRisoni <vanguelisk@gmail.com>
2020-09-09 08:15:55 +0300

    pretty fy

--- src/Home.js
+++ src/Home.js
@@ -1,62 +1,52 @@
 import React from "react";
-import axios from 'axios';
-
+import axios from "axios";
 
 class Home extends React.Component {
-    constructor(props) 
-    {
-        super(props);
-        this.state = {
-            cusines:[],
-            fetched:false
-        }
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      cusines: [],
+      fetched: false
+    };
+  }
 
-   componentDidMount() {
-        const self = this;
-        console.log(process.env)
+  componentDidMount() {
+    const self = this;
+    console.log(process.env);
 
-        axios.get(`${process.env.REACT_APP_API_ENDPOINT}/api/browse`)
-            .then(responseObj => {
-        
-                self.setState({
-                    cusines: responseObj.data.cusines,
-                    fetched: true
-                })
+    axios
+      .get(`${process.env.REACT_APP_API_ENDPOINT}/api/browse`)
+      .then(responseObj => {
+        self.setState({
+          cusines: responseObj.data.cusines,
+          fetched: true
+        });
+      });
+  }
 
-            });
-
-    }
-
-    render() {
-
-     if (this.state.fetched) {
-  return (
-    <section>
-         <div className="row">
+  render() {
+    if (this.state.fetched) {
+      return (
+        <section>
+          <div className="row">
             <div className="col-2">
-
-            
-                {this.state.cusines.map(cus => {
-                    return (<div key={cus.id} className="row">
-                        <div className="col-2">
-                            {cus.name}
-                        </div>
-                    </div>)
-                })}
-            
-
+              {this.state.cusines.map(cus => {
+                return (
+                  <div key={cus.id} className="row">
+                    <div className="col-2">{cus.name}</div>
+                  </div>
+                );
+              })}
             </div>
 
-
-         </div>
-    </section>
-  );
-            }
-            else {
-                return (<p> Wait ...</p>)
-            }
-        }
+            <div className="col-8"></div>
+          </div>
+        </section>
+      );
+    } else {
+      return <p> Wait ...</p>;
+    }
+  }
 }
 
 export default Home;
