535864b1439c3abae3226934f58bf0e8bde0a91e
MrRisoni <vanguelisk@gmail.com>
2020-09-09 07:39:59 +0300

    render cuisines

--- src/Home.js
+++ src/Home.js
@@ -2,12 +2,61 @@ import React from "react";
 import axios from 'axios';
 
 
-function Home() {
+class Home extends React.Component {
+    constructor(props) 
+    {
+        super(props);
+        this.state = {
+            cusines:[],
+            fetched:false
+        }
+    }
+
+   componentDidMount() {
+        const self = this;
+        console.log(process.env)
+
+        axios.get(`${process.env.REACT_APP_API_ENDPOINT}/api/browse`)
+            .then(responseObj => {
+        
+                self.setState({
+                    cusines: responseObj.data.cusines,
+                    fetched: true
+                })
+
+            });
+
+    }
+
+    render() {
+
+     if (this.state.fetched) {
   return (
     <section>
-      Favorite restaurants
+         <div className="row">
+            <div className="col-2">
+
+            
+                {this.state.cusines.map(cus => {
+                    return (<div key={cus.id} className="row">
+                        <div className="col-2">
+                            {cus.name}
+                        </div>
+                    </div>)
+                })}
+            
+
+            </div>
+
+
+         </div>
     </section>
   );
+            }
+            else {
+                return (<p> Wait ...</p>)
+            }
+        }
 }
 
 export default Home;
